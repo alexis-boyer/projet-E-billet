27336ea452584ec02addf6e43167a6a9454a2165
Hakim <hakim.amraoui@outlook.fr>
2021-04-21 20:03:35 +0200

    J'ai enlevé les controllers y a un probleme je vais chercher pourquoi.

--- src/main/java/fr/univtours/projet/controller/UtilisateurController.java
+++ src/main/java/fr/univtours/projet/controller/UtilisateurController.java
@@ -12,8 +12,6 @@ import java.util.List;
 public class UtilisateurController {
 
 
-    @Autowired
-    UtilisateurJpaRepository utilisateurRep;
 
 
 
--- src/main/java/fr/univtours/projet/dao/UtilisateurJpaRepository.java
+++ src/main/java/fr/univtours/projet/dao/UtilisateurJpaRepository.java
@@ -3,6 +3,7 @@ package fr.univtours.projet.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import fr.univtours.projet.entities.Utilisateur;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Service;
 
 @RepositoryRestResource
 public interface UtilisateurJpaRepository extends JpaRepository<Utilisateur,Integer> {
