c4f5d24b9e772338731bfd968d4f5481a9df1761
HakimAmraoui <hakim.amraoui@outlook.fr>
2021-05-20 20:28:51 +0200

    Bug fixes

--- src/main/java/fr/univtours/projet/service/InitEbilletDatas.java
+++ src/main/java/fr/univtours/projet/service/InitEbilletDatas.java
@@ -5,10 +5,8 @@ import fr.univtours.projet.entities.Evenement;
 import fr.univtours.projet.entities.Ticket;
 import fr.univtours.projet.entities.Utilisateur;
 
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
 
 public class InitEbilletDatas {
 
@@ -17,40 +15,40 @@ public class InitEbilletDatas {
     ArrayList<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
     ArrayList<Evenement> evenements = new ArrayList<Evenement>();
 
-    Artiste a1;
-    Artiste a2;
-    Artiste a3;
-    Artiste a4;
-    Artiste a5;
-    Artiste a6;
-    Artiste a7;
-    Artiste a8;
-    Artiste a9;
-    Artiste a10;
-    Artiste a11;
-    Artiste a12;
-    Artiste a13;
-    Artiste a14;
-    Artiste a15;
-    Artiste a16;
-    Artiste a17;
-    Artiste a18;
-    Artiste a19;
-    Artiste a20;
-    Artiste a21;
-    Artiste a22;
-    Artiste a23;
-    Artiste a24;
-    Artiste a25;
-    Artiste a26;
-    Artiste a27;
-    Artiste a28;
-    Artiste a29;
-    Artiste a30;
-
+    Artiste a1 = new Artiste("Jul");
+    Artiste a2 = new Artiste("Michael Jordan");
+    Artiste a3 = new Artiste("Elvis Presley");
+    Artiste a4 = new Artiste("Michael Jackson");
+    Artiste a5 = new Artiste("Céline Dion");
+    Artiste a6 = new Artiste("Bob Marley");
+    Artiste a7 = new Artiste("Stevie Wonder");
+    Artiste a8 = new Artiste("James Brown");
+    Artiste a9 = new Artiste("Aretha Franklin");
+    Artiste a10 = new Artiste("Ray Charles");
+    Artiste a11 = new Artiste("Sam Cooke");
+    Artiste a12 = new Artiste("John Lennon");
+    Artiste a13 = new Artiste("Marvin Gaye");
+    Artiste a14 = new Artiste("Bob Dylan");
+    Artiste a15 = new Artiste("Otis Redding");
+    Artiste a16 = new Artiste("Stevie Wonder");
+    Artiste a17 = new Artiste("Chris Brown");
+    Artiste a18 = new Artiste("Soprano");
+    Artiste a19 = new Artiste("Abou Tall");
+    Artiste a20 = new Artiste("Gim's");
+    Artiste a21 = new Artiste("Tayc");
+    Artiste a22 = new Artiste("Dadju");
+    Artiste a23 = new Artiste("Slimane");
+    Artiste a24 = new Artiste("Youssoupha");
+    Artiste a25 = new Artiste("Kery James");
+    Artiste a26 = new Artiste("Amel Bent");
+    Artiste a27 = new Artiste("Black M");
+    Artiste a28 = new Artiste("Imen");
+    Artiste a29 = new Artiste("Vita");
+    Artiste a30 = new Artiste("Franck");
     Artiste adama = new Artiste("Adama AKA Yoba");
     Artiste alexis = new Artiste("Alexis AKA KillerDu79");
-    Artiste hakim = new  Artiste("Hakim AKA Zurkon Sama");
+    Artiste hakim = new Artiste("Hakim AKA Zurkon Sama");
+
 
     // Ticket
 
@@ -83,84 +81,22 @@ public class InitEbilletDatas {
     Evenement e20;
 
 
-
     protected ArrayList<Artiste> getArtistesList() {
-
-        a1 = new Artiste("Jul");
-        a2 = new Artiste("Michael Jordan");
-        a3 = new Artiste("Elvis Presley");
-        a4 = new Artiste("Michael Jackson");
-        a5 = new Artiste("Céline Dion");
-        a6 = new Artiste("Bob Marley");
-        a7 = new Artiste("Stevie Wonder");
-        a8 = new Artiste("James Brown");
-        a9 = new Artiste("Aretha Franklin");
-        a10 = new Artiste("Ray Charles");
-        a11 = new Artiste("Sam Cooke");
-        a12 = new Artiste("John Lennon");
-        a13 = new Artiste("Marvin Gaye");
-        a14 = new Artiste("Bob Dylan");
-        a15 = new Artiste("Otis Redding");
-        a16 = new Artiste("Stevie Wonder");
-        a17 = new Artiste("Chris Brown");
-        a18 = new Artiste("Soprano");
-        a19 = new Artiste("Abou Tall");
-        a20 = new Artiste("Gim's");
-        a21 = new Artiste("Tayc");
-        a22 = new Artiste("Dadju");
-        a23 = new Artiste("Slimane");
-        a24 = new Artiste("Youssoupha");
-        a25 = new Artiste("Kerry James");
-        a26 = new Artiste("Amel Bent");
-        a27 = new Artiste("Black M");
-        a28 = new Artiste("Imen");
-        a29 = new Artiste("Vita");
-        a30 = new Artiste("franck");
-
-        artistes.add(a1);
-        artistes.add(a2);
-        artistes.add(a3);
-        artistes.add(a4);
-        artistes.add(a5);
-        artistes.add(a6);
-        artistes.add(a7);
-        artistes.add(a8);
-        artistes.add(a9);
-        artistes.add(a10);
-        artistes.add(a11);
-        artistes.add(a12);
-        artistes.add(a13);
-        artistes.add(a14);
-        artistes.add(a15);
-        artistes.add(a16);
-        artistes.add(a17);
-        artistes.add(a18);
-        artistes.add(a19);
-        artistes.add(a20);
-        artistes.add(a21);
-        artistes.add(a22);
-        artistes.add(a23);
-        artistes.add(a24);
-        artistes.add(a25);
-        artistes.add(a26);
-        artistes.add(a27);
-        artistes.add(a28);
-        artistes.add(a29);
-        artistes.add(a30);
 
         return artistes;
 
     }
 
-    protected ArrayList<Ticket> getTicketsList(){
+    protected ArrayList<Ticket> getTicketsList() {
 
         return tickets;
 
     }
-    protected ArrayList<Utilisateur> getUtilisateursList(){
+
+    protected ArrayList<Utilisateur> getUtilisateursList() {
 
         u1 = new Utilisateur("Zinedine", "Zidane", "Meilleur", "Joueur", "Admin");
-        u2 = new Utilisateur("client", "client", "client", "client","Client");
+        u2 = new Utilisateur("client", "client", "client", "client", "Client");
         u3 = new Utilisateur("admin", "admin", "admin", "admin", "Admin");
 
         utilisateurs.add(u1);
@@ -169,16 +105,17 @@ public class InitEbilletDatas {
 
         return utilisateurs;
     }
-    protected ArrayList<Evenement> getEvenementsList(){
-        e1 = new Evenement(1000, "Paris", "Showcase", "20/05/2021", 125);
-        e2 = new Evenement(6, "Guingamp", "One Man Show", "28/05/2022", 0.50);
+
+    protected ArrayList<Evenement> getEvenementsList() {
+        e1 = new Evenement(1, "Paris", "Showcase", "20/05/2021", 125);
+        e2 = new Evenement(2, "Guingamp", "One Man Show", "28/05/2022", 0.50);
         e3 = new Evenement(60, "Tours", "Street Art Cinema", "01/06/2021", 1.30);
         e4 = new Evenement(90, "Angers", "City of Gold Art Festival", "20/06/2021", 5.50);
         e5 = new Evenement(200, "Bordeaux", "The big Picture Festival", "05/07/2021", 4.90);
         e6 = new Evenement(140, "Paris", "Graffiti fest", "15/07/2021", 6);
         e7 = new Evenement(65, "Orleans", "The Game Day", "23/07/2021", 3);
         e8 = new Evenement(120, "Limoge", "Big show", "29/07/2021", 13);
-        e9 = new Evenement(99, "Rennes", "Graffiti Night","03/08/2021", 1.80);
+        e9 = new Evenement(99, "Rennes", "Graffiti Night", "03/08/2021", 1.80);
         e10 = new Evenement(260, "Reims", "Skatt Day", "06/08/2021", 2.30);
         e11 = new Evenement(30, "Caen", "La Street française", "10/08/2021", 1.30);
         e12 = new Evenement(80, "Blois", "Meeting of Styles", "18/08/2021", 130);
@@ -201,78 +138,76 @@ public class InitEbilletDatas {
         hs2.add(adama);
         e2.setArtistes(hs2);
 
-        // TODO: 12/05/2021 J'ai commente toutes les lignes qui posent problemes, les artistes instanties dans getArtistes font une erreur
-//        HashSet<Artiste> hs3 = new HashSet<>();
-//        hs3.add(a1);
-//        hs3.add(a2);
-//        hs3.add(a3);
-//        hs3.add(a4);
-//        e3.setArtistes(hs3);
-//
-//
-//
-//        HashSet<Artiste> hs4 = new HashSet<>();
-//        hs4.add(a5);
-//        hs4.add(a6);
-//        hs4.add(a7);
-//        hs4.add(a8);
-//        hs4.add(a9);
-//        hs4.add(a10);
-//        e4.setArtistes(hs4);
-//
-//
-//
-//        HashSet<Artiste> hs5 = new HashSet<>();
-//        hs5.add(a11);
-//        hs5.add(a12);
-//        hs5.add(a13);
-//        hs5.add(a14);
-//        hs5.add(a15);
-//        e5.setArtistes(hs5);
-//
-//
-//        HashSet<Artiste> hs6 = new HashSet<>();
-//        hs6.add(a16);
-//        hs6.add(a17);
-//        hs6.add(a18);
-//        hs6.add(a19);
-//        hs6.add(a20);
-//        e6.setArtistes(hs6);
-//
-//        HashSet<Artiste> hs7 = new HashSet<>();
-//        hs7.add(a21);
-//        hs7.add(a22);
-//        hs7.add(a23);
-//        hs7.add(a24);
-//        hs7.add(a25);
-//        e7.setArtistes(hs7);
-//
-//
-//        HashSet<Artiste> hs8 = new HashSet<>();
-//        hs8.add(a26);
-//        hs8.add(a27);
-//        hs8.add(a28);
-//        hs8.add(a29);
-//        hs8.add(a30);
-//        e8.setArtistes(hs8);
-//
-//
-//        HashSet<Artiste> hs9 = new HashSet<>();
-//        hs9.add(a16);
-//        hs9.add(a20);
-//        hs9.add(a11);
-//        hs9.add(a7);
-//        hs9.add(a30);
-//        e9.setArtistes(hs9);
-//
-//        HashSet<Artiste> hs10 = new HashSet<>();
-//        hs10.add(a16);
-//        hs10.add(a20);
-//        hs10.add(a11);
-//        hs10.add(a7);
-//        hs10.add(a30);
-//        e10.setArtistes(hs10);
+        HashSet<Artiste> hs3 = new HashSet<>();
+        hs3.add(a1);
+        hs3.add(a2);
+        hs3.add(a3);
+        hs3.add(a4);
+        e3.setArtistes(hs3);
 
+
+
+        HashSet<Artiste> hs4 = new HashSet<>();
+        hs4.add(a5);
+        hs4.add(a6);
+        hs4.add(a7);
+        hs4.add(a8);
+        hs4.add(a9);
+        hs4.add(a10);
+        e4.setArtistes(hs4);
+
+
+
+        HashSet<Artiste> hs5 = new HashSet<>();
+        hs5.add(a11);
+        hs5.add(a12);
+        hs5.add(a13);
+        hs5.add(a14);
+        hs5.add(a15);
+        e5.setArtistes(hs5);
+
+
+        HashSet<Artiste> hs6 = new HashSet<>();
+        hs6.add(a16);
+        hs6.add(a17);
+        hs6.add(a18);
+        hs6.add(a19);
+        hs6.add(a20);
+        e6.setArtistes(hs6);
+
+        HashSet<Artiste> hs7 = new HashSet<>();
+        hs7.add(a21);
+        hs7.add(a22);
+        hs7.add(a23);
+        hs7.add(a24);
+        hs7.add(a25);
+        e7.setArtistes(hs7);
+
+
+        HashSet<Artiste> hs8 = new HashSet<>();
+        hs8.add(a26);
+        hs8.add(a27);
+        hs8.add(a28);
+        hs8.add(a29);
+        hs8.add(a30);
+        e8.setArtistes(hs8);
+
+
+        HashSet<Artiste> hs9 = new HashSet<>();
+        hs9.add(a16);
+        hs9.add(a20);
+        hs9.add(a11);
+        hs9.add(a7);
+        hs9.add(a30);
+        e9.setArtistes(hs9);
+
+        HashSet<Artiste> hs10 = new HashSet<>();
+        hs10.add(a16);
+        hs10.add(a20);
+        hs10.add(a11);
+        hs10.add(a7);
+        hs10.add(a30);
+        e10.setArtistes(hs10);
 
 
         evenements.add(e1);
